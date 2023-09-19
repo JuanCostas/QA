352200d7d4a54779d5edd1092d4e6b522e9c0ac4
Juan Ignacio Costas <juaniganciocostas@gmail.com>
2023-09-18 21:01:15 -0300

    Subiendo a git

---
+++ Manejo_de_Errores/src/manejo_de_errores/Manejo_de_Errores.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejo_de_errores;
+
+public class Manejo_de_Errores {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
