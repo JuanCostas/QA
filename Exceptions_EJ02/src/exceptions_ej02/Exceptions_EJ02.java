98f2c145c8a4da466b7f4a0c7cf2b7b4933ee7c4
Juan Ignacio Costas <juaniganciocostas@gmail.com>
2023-09-19 20:35:26 -0300

    Exceptions

---
+++ Exceptions_EJ02/src/exceptions_ej02/Exceptions_EJ02.java
@@ -0,0 +1,17 @@
+/*
+    Definir una Clase que contenga algún tipo de dato de tipo array y agregue el código para
+generar y capturar una excepción ArrayIndexOutOfBoundsException (índice de arreglo fuera
+de rango).
+ */
+package exceptions_ej02;
+
+import Entidades.Arreglo;
+
+public class Exceptions_EJ02 {
+
+    public static void main(String[] args) {
+        Arreglo a = new Arreglo();
+        a.mostrar();
+    }
+    
+}
